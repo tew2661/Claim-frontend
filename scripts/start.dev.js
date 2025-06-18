3c050d08ce2509de4726a47e0532f27502277cfe
paponwit2661 <paponwitmingsamorn@PAPONWIT-M3-PRO.local>
2025-06-19 00:07:25 +0700

    fix run start project and defualt redirect to pages

---
+++ scripts/start.dev.js
@@ -0,0 +1,11 @@
+
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+const { execSync } = require('child_process');
+
+const env = dotenv.config();
+dotenvExpand.expand(env);
+
+const port = process.env.NEXT_PORT || 3000;
+
+execSync(`next dev --turbopack -p ${port}`, { stdio: 'inherit' });
