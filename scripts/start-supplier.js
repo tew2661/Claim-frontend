a5a0f4caf2f9817d1c019f77bcfcf4406965136e
paponwit2661 <paponwitmingsamorn@PAPONWIT-M3-PRO.local>
2025-06-19 09:05:01 +0700

    fix script run

--- scripts/start-supplier.js
+++ scripts/start-supplier.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 const { execSync } = require('child_process');
 
-const env = dotenv.config();
+const env = dotenv.config({ path: '.env.supplier' });
 dotenvExpand.expand(env);
 
 const port = process.env.NEXT_PORT || 3000;
